b6c8b8d850c91d8de8979bb1dc831464f78cfd94
Matt Vague <mattvague@gmail.com>
2021-03-13 12:50:35 -0800

    Add ci env

---
+++ config/environments/ci.rb
@@ -0,0 +1,20 @@
+require_relative 'development'
+ 
+Rails.application.configure do
+  config.log_level = :debug
+
+  config.action_controller.perform_caching = false
+
+  config.cache_store = :null_store
+
+  config.cache_classes = false
+  config.action_view.cache_template_loading = false
+
+  config.active_job.queue_adapter = :async
+
+  if ENV['RAILS_LOG_TO_STDOUT'].present?
+    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = config.log_formatter
+    config.logger    = ActiveSupport::TaggedLogging.new(logger)
+  end
+end
