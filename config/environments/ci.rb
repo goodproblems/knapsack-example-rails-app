1cf201eb8e5c5f4939be95b23a79f20f4754b20d
Matt Vague <mattvague@gmail.com>
2021-03-13 13:05:09 -0800

    Try and fix secret key base

--- config/environments/ci.rb
+++ config/environments/ci.rb
@@ -10,6 +10,9 @@ Rails.application.configure do
   config.cache_classes = false
   config.action_view.cache_template_loading = false
 
+  config.require_master_key = false
+  config.read_encrypted_secrets = false
+
   config.active_job.queue_adapter = :async
 
   if ENV['RAILS_LOG_TO_STDOUT'].present?
